f7059c65fca61d51614129962024d1d3807e3941
tarik-oz <tarikoz201007@gmail.com>
2025-05-23 05:53:04 +0300

    feat(domain): add base entity and core domain entities

---
+++ Core/EcommerceApi.Domain/Entities/Category.cs
@@ -0,0 +1,29 @@
+﻿using EcommerceApi.Domain.Common;
+using EcommerceApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceApi.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public Category()
+        {
+            
+        }
+        public Category(int parentId, string name, int priority)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priority = priority;
+        }
+        public required int ParentId { get; set; } = 0;
+        public required string Name { get; set; } = string.Empty;
+        public required int Priority { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Core/EcommerceApi.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using EcommerceApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceApi.Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int BrandId { get; set; }
+        public required decimal Price { get; set; }
+        public required int Discount { get; set; }
+        public Brand Brand { get; set; }
+        public ICollection<Category> Categories { get; set; }
+    }
+}
