4bb3101a32c7bbb8840ee3fd0b7fdda43dca76e6
tarik-oz <tarikoz201007@gmail.com>
2025-05-29 23:59:36 +0300

    feat(application, api): add brand create, read operations for Redis cache test with 1 million sample data

---
+++ Core/EcommerceApi.Application/Features/Brands/Queries/GetAllBrands/GetAllBrandsHandler.cs
@@ -0,0 +1,28 @@
+﻿using EcommerceApi.Application.Bases;
+using EcommerceApi.Application.Interfaces.AutoMapper;
+using EcommerceApi.Application.Interfaces.UnitOfWorks;
+using EcommerceApi.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceApi.Application.Features.Brands.Queries.GetAllBrands
+{
+    public class GetAllBrandsQueryHandler : BaseHandler, IRequestHandler<GetAllBrandsQueryRequest, IList<GetAllBrandsQueryResponse>>
+    {
+        public GetAllBrandsQueryHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+        }
+
+        public async Task<IList<GetAllBrandsQueryResponse>> Handle(GetAllBrandsQueryRequest request, CancellationToken cancellationToken)
+        {
+            var brands = await unitOfWork.GetReadRepository<Brand>().GetAllAsync();
+
+            return mapper.Map<GetAllBrandsQueryResponse, Brand>(brands);
+        }
+    }
+}
---
+++ Presentation/EcommerceApi.Api/Controllers/BrandController.cs
@@ -0,0 +1,34 @@
+﻿using EcommerceApi.Application.Features.Brands.Commands.CreateBrand;
+using EcommerceApi.Application.Features.Brands.Queries.GetAllBrands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceApi.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public BrandController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBrand(CreateBrandCommandRequest request)
+        {
+            await mediator.Send(request);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBrands()
+        {
+            var response = await mediator.Send(new GetAllBrandsQueryRequest());
+            return Ok(response);
+        }
+    }
+}
